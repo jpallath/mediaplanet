11dddbe986a99756a267563eb8e14d8521c19acd
Jerry Pallath <jpallath@nyu.edu>
2015-09-29 14:59:09 -0400

    controllers and mongo working

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+  var userSchema = Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    name:     {type: String, required: true}
+  })
+
+  var User = mongoose.model("User", userSchema);
+
+  module.exports = User;
